50b34cbd38ecbef213eb027d6f46e0822a115ed2
mcfelicita <mietchellefelicita@gmail.com>
2025-05-07 02:51:03 -0700

    third commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,5 +12,6 @@ def home():
         "Exam: Semi-Final Exam"
     )
 
-if __name__ == '__main_':
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
